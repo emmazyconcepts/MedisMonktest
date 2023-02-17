8605d5a033a1a3572277c3a5b72fcf264f4a40b8
ekeemmanuel <emmazy100@gmail.com>
2023-02-17 22:12:45 +0100

    over

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+  var newPosition = 0;
+  var currentId = 0;
+  var prevId;
+  var arrowNext = document.getElementById("front_arrow");
+  var arrowPrev = document.getElementById("back_arrow");
+  var joindiv = document.getElementById("section-9");
+  var navigation = document.getElementById("content_nav");
+  var bgImage = document.getElementById("bg_img");
+  var coordinatesArray = [0, 125, 225, 350, 495, 625, 780, 940, 940, 1060];
+  const bgUnit = "vh";
+
+  //   handles mobile view or desktop view
+
+  /* Storing user's device details in a variable*/
+  let details = navigator.userAgent;
+
+  /* Creating a regular expression
+  containing some mobile devices keywords
+  to search it in details string*/
+  let regexp = /android|iphone|kindle|ipad/i;
+
+  /* Using test() method to search regexp in details
+  it returns boolean value*/
+  let isMobileDevice = regexp.test(details);
+
+  if (isMobileDevice) {
+    coordinatesArray = [0, 200, 300, 370, 525, 720, 860, 1060, 1060, 1110];
+  } else {
+    coordinatesArray = [0, 125, 225, 350, 495, 625, 780, 940, 940, 1060];
+  }
+
   // handles the preloader
   function vanish() {
     document.getElementById("loader").classList.add("disppear");
     document.getElementById("loader").classList.add("img");
   }
 
-  setTimeout(vanish, 5000);
-
-  var arrowNext = document.getElementById("front_arrow");
-  var arrowPrev = document.getElementById("back_arrow");
-  var joindiv = document.getElementById("section-9");
-
-  var bgImage = document.getElementById("bg_img");
-  var navigation = document.getElementById("content_nav");
-  var newPosition = 0;
-  var currentId = 0;
-  var prevId;
-
-  const coordinatesArray = [0, 125, 225, 350, 495, 625, 780, 900, 940, 1060];
-  const lengthUnit = "vh";
+  setTimeout(vanish, 500);
 
   function getCoordinates(coordinatesArray, pageId) {
     return `-${coordinatesArray[pageId]}`;
@@ -39,22 +57,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
   navigation.addEventListener("click", (event) => {
     const coordinates = getCoordinates(coordinatesArray, event.target.id);
     currentId = event.target.id;
+
     renderPage(coordinates);
   });
 
+  // handles display of arrows depending on the page
+
   function checkArrowVisibility() {
-    arrowNext.style.visibility = currentId === 9 ? "hidden" : "visible";
-    joindiv.style.visibility = currentId === 9 ? "visible" : "hidden";
-    arrowPrev.style.visibility = currentId === 0 ? "hidden" : "visible";
+    arrowNext.style.visibility = currentId == 9 ? "hidden" : "visible";
+    arrowPrev.style.visibility = currentId == 0 ? "hidden" : "visible";
+    joindiv.style.visibility = currentId == 9 ? "visible" : "hidden";
   }
 
   function renderPage(coordinates) {
-    // set new bg position
+    // set new bg to a new position using the coordinates
     newPosition = coordinates;
-    bgImage.style["transform"] =
-      "translate(" + newPosition + lengthUnit + ", 0)";
+    bgImage.style["transform"] = "translate(" + newPosition + bgUnit + ", 0)";
 
-    // hide previous content
+    // hide previous section
     if (prevId != null) {
       var prevArticleId = "section-" + prevId,
         prevArticle = document.getElementById(prevArticleId),
@@ -66,20 +86,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
       prevNavButton.className = "";
     }
 
-    // display current page on navigation
-    var newNavButton = document.getElementById(currentId);
-    newNavButton.className = "listitem--clicked";
+    // shows current page or item clicked
+    const newNavButton = document.getElementById(currentId);
+    newNavButton.classList.add(`listitem--clicked`);
 
-    // display current page contents
-    setTimeout(function () {
-      var articleId = "section-" + currentId,
-        currentArticle = document.getElementById(articleId);
-
+    // timer to display current page section
+    setTimeout(() => {
+      const articleId = `section-${currentId}`;
+      const currentArticle = document.getElementById(articleId);
       currentArticle.style.opacity = 1;
     }, 500);
 
     checkArrowVisibility();
-    // set previous page id to the current one
     prevId = currentId;
   }
 
