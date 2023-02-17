785b5b69eec1ab5980b9f10cce37ebd23c84dbaf
ekeemmanuel <emmazy100@gmail.com>
2023-02-17 06:42:07 +0100

    style

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// handles the preloader
 function vanish() {
   document.getElementById("loader").classList.add("disppear");
-  document.getElementById("loader").classList.add("imge");
+  document.getElementById("loader").classList.add("img");
 }
 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//   setTimeout(vanish, 50000);
-// });
+document.addEventListener("DOMContentLoaded", function (event) {
+  setTimeout(vanish, 500);
+});
